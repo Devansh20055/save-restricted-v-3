62b00a9f7c53fb032c585bff798117d703ed9b67
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-23 12:42:49 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,49 +1,4 @@
-#Github.com/Vasusen-code
-
-import os
-from .. import bot as Drone
-from telethon import events, Button
-
-from ethon.mystarts import start_srb
-    
-S = '/' + 's' + 't' + 'a' + 'r' + 't'
-
-@Drone.on(events.callbackquery.CallbackQuery(data="set"))
-async def sett(event):    
-    Drone = event.client                    
-    button = await event.get_message()
-    msg = await button.get_reply_message() 
-    await event.delete()
-    async with Drone.conversation(event.chat_id) as conv: 
-        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
-        x = await conv.get_reply()
-        if not x.media:
-            xx.edit("No media found.")
-        mime = x.file.mime_type
-        if not 'png' in mime:
-            if not 'jpg' in mime:
-                if not 'jpeg' in mime:
-                    return await xx.edit("No image found.")
-        await xx.delete()
-        t = await event.client.send_message(event.chat_id, 'Trying.')
-        path = await event.client.download_media(x.media)
-        if os.path.exists(f'{event.sender_id}.jpg'):
-            os.remove(f'{event.sender_id}.jpg')
-        os.rename(path, f'./{event.sender_id}.jpg')
-        await t.edit("Temporary thumbnail saved!  \n ⚡️⚡️ JOIN MY CHANNEL :-  @TEAM_SILENT_KING FOR MORE ⚡️⚡️")
-        
-@Drone.on(events.callbackquery.CallbackQuery(data="rem"))
-async def remt(event):  
-    Drone = event.client            
-    await event.edit('Trying.')
-    try:
-        os.remove(f'{event.sender_id}.jpg')
-        await event.edit('Removed!')
-    except Exception:
-        await event.edit("No thumbnail saved.")                        
-  
-@Drone.on(events.NewMessage(incoming=True, pattern="/start"))async def start(event): await event.reply(f'{st}', #Github.com/Vasusen-code
-
+                     
 import os
 from .. import bot as Drone
 from telethon import events, Button
@@ -117,8 +72,7 @@ just send your Post link i will give you that post without Downloading.
 \n\n<b><u>For Private Restricted Channel contents.</b></u>\nTo get private restricted Channel contents, 
 First send me Channel invite link so that i can join your channel after that send me post link of your restricted Channel to get that post. ,
  <B><U><I> JOIN :- @TEAM_SILENT_KING TO USE ME </B></U></I>""", parse_mode="HTML")
-    #end help Message
-                                                                                                       buttons=[ [Button.url("⚡️⚡️ Updates Channel ⚡️⚡️", url="https://t.me/TEAM_SILENT_KING"),
+    #end help Messag                                                                              
                                                                                                                   Button.url("👩🏻‍💻👨🏻‍💻 Support Group 👩🏻‍💻👨🏻‍💻", url="https://t.me/OFF_CHATS")],
                                                                                                                 [Button.url("🔰🔰 YouTube Channel🔰🔰", url="https://www.youtube.com/channel/UC28Z7OuZiKuIZ-kFxNkG4Kww")], ])
     
